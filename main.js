a5f00cc4d163af80403bbda0739da787a21f0eab
“Saki <sakisandra.c@gmail.com>
2023-04-13 18:34:54 -0600

    Add game view and hidden class

---
+++ main.js
@@ -0,0 +1,13 @@
+let classicMode = document.querySelector('#classic');
+let difficultMode = document.querySelector('#difficult');
+
+classicMode.addEventListener('click', classicModeGame)
+difficultMode.addEventListener('click', difficultModeGame)
+
+function classicModeGame() {
+   
+}
+
+function difficultModeGame() {
+    console.log('difficult!')
+}
